e017fb0020bd6679f632285c5d1b650e5c89cce1
Guilherme <guiihh532@gmail.com>
2021-03-11 11:04:28 -0300

    Project created

---
+++ src/test/java/com/gomlek/coursetwo/CoursejpaTwoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomlek.coursetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursejpaTwoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
